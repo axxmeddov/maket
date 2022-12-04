c12cff5af111255b1d08203c4dfae7c792af9f50
axxmeddov <rovsen.brat@mail.ru>
2022-12-05 01:14:51 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+let money = 0
+
+function giveMoney(x) {
+  return money+=x;
+}
+
+let moneyy = 0
+
+function givemoneyy(y) {
+    return moneyy+=y
+}
+
+let moneyyy = 0 
+
+function givemoneyyy(z) {
+    return moneyyy+=z
+}
